77c26d71376f154c442a4b8776a8628311ca0b5f
Ji Park <ji.park@jpnws.com>
2024-05-06 22:39:58 -0700

    chore: Add infrastructure files and configuration
    This commit adds the necessary infrastructure files and configuration for the project. It includes the `.npmignore` and `.gitignore` files in the `infra` directory, as well as the `config.json`, `jest.config.js`, `bin/infra.ts`, `cdk.context.json`, `README.md`, `package.json`, `test/infra.test.ts`, and `tsconfig.json` files. These files are essential for setting up the CDK TypeScript project and running the necessary commands for building, testing, and deploying the infrastructure stack.
    
    Please note that this commit message adheres to the conventional commit format, with the type `chore` indicating a task related to build or tooling.

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,178 @@
+import * as cdk from "aws-cdk-lib";
+import * as cmg from "aws-cdk-lib/aws-certificatemanager";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as elb from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as lgp from "aws-cdk-lib/aws-logs";
+import * as r53 from "aws-cdk-lib/aws-route53";
+import * as rtg from "aws-cdk-lib/aws-route53-targets";
+import * as smg from "aws-cdk-lib/aws-secretsmanager";
+import { Construct } from "constructs";
+import { resolve } from "path";
+import {
+  backend_subdomain as backendSubdomain,
+  domain_name as domainName,
+} from "../../config.json";
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // * =====================================
+    // * Virtual Private Cloud (VPC)
+    // * =====================================
+
+    // Define the VPC and Security Group. You must define the public subnet
+    // where containerized apps are deployed and the private subnet where
+    // DocumentDB is deployed.
+    const vpc = new ec2.Vpc(this, "VPC", {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "private",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    // * =====================================
+    // * Route 53
+    // * =====================================
+
+    // Define the Route 53 hosted zone.
+    const hostedZone = r53.HostedZone.fromLookup(this, "HostedZone", {
+      domainName,
+    });
+
+    // * =====================================
+    // * Certificate Manager
+    // * =====================================
+
+    const certificate = new cmg.Certificate(this, "Certificate", {
+      // Domain name to be secured by the certificate
+      domainName,
+      // Use DNS validation for the certificate.
+      validation: cmg.CertificateValidation.fromDns(hostedZone),
+      // Secure all subdomains under the primary domain
+      subjectAlternativeNames: [`*.${domainName}`],
+    });
+
+    // * =====================================
+    // * Elastic Container Service (ECS)
+    // * =====================================
+
+    // Create ECS task definition.
+    const ecsTaskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "TaskDefinition"
+    );
+
+    // Create a new secret for PAYLOAD_SECRET with a random value
+    const payloadSecret = new smg.Secret(this, "PayloadSecret", {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({}),
+        generateStringKey: "payloadSecret",
+        excludeCharacters: '"@/\\ ',
+      },
+    });
+
+    // Create a log group for the ECS task.
+    const logGroup = new lgp.LogGroup(this, "ECSLogGroup", {
+      retention: lgp.RetentionDays.ONE_DAY,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Add a container to the task definition.
+    ecsTaskDefinition.addContainer("PayloadContainer", {
+      image: ecs.ContainerImage.fromAsset(resolve(__dirname, "../../server")),
+      memoryLimitMiB: 512,
+      cpu: 256,
+      secrets: {
+        PAYLOAD_SECRET: ecs.Secret.fromSecretsManager(
+          payloadSecret,
+          "payloadSecret"
+        ),
+      },
+      portMappings: [
+        {
+          // Expose the container port.
+          containerPort: 3000,
+        },
+      ],
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: "ecs",
+        logGroup,
+      }),
+    });
+
+    // Define the security group for the ECS cluster.
+    const ecsSecurityGroup = new ec2.SecurityGroup(this, "ECSSecurityGroup", {
+      vpc: vpc,
+    });
+
+    // Create an ECS cluster.
+    const ecsCluster = new ecs.Cluster(this, "ECSCluster", { vpc });
+
+    // Create an ECS service.
+    const ecsService = new ecs.FargateService(this, "ECSService", {
+      cluster: ecsCluster,
+      taskDefinition: ecsTaskDefinition,
+      securityGroups: [ecsSecurityGroup],
+      assignPublicIp: true,
+      vpcSubnets: vpc.selectSubnets({ subnetGroupName: "public" }),
+    });
+
+    // * =====================================
+    // * Application Load Balancer and ECS
+    // * =====================================
+
+    // Application load balancer.
+    const loadBalancer = new elb.ApplicationLoadBalancer(this, "LoadBalancer", {
+      vpc: vpc,
+      internetFacing: true,
+    });
+
+    // Add a listener to the load balancer and targets to listen.
+    const loadBalancerListener = loadBalancer.addListener("Listener", {
+      port: 443,
+      certificates: [certificate],
+    });
+
+    // Add the ECS service as a target to the load balancer listener.
+    loadBalancerListener.addTargets("ECS", {
+      port: 443,
+      targets: [ecsService],
+      healthCheck: {
+        interval: cdk.Duration.seconds(30),
+        path: "/health",
+        timeout: cdk.Duration.seconds(5),
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 3,
+        protocol: elb.Protocol.HTTPS,
+      },
+    });
+
+    // * =====================================
+    // * DNS
+    // * =====================================
+
+    // DNS A record pointing to the load balancer
+    new r53.ARecord(this, "BackendPayloadARecord", {
+      zone: hostedZone,
+      target: r53.RecordTarget.fromAlias(
+        new rtg.LoadBalancerTarget(loadBalancer)
+      ),
+      recordName: `${backendSubdomain}.${domainName}`,
+    });
+
+    // Output the DNS name of the load balancer.
+    new cdk.CfnOutput(this, "BackendPayloadURL", {
+      value: loadBalancer.loadBalancerDnsName,
+    });
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,23 @@ app.get("/", (_req: Request, res: Response) => {
   res.send("Hello World!123123");
 });
 
+// Adding a new route to display the PAYLOAD_SECRET
+app.get("/secret", (_req: Request, res: Response) => {
+  const secret = process.env["PAYLOAD_SECRET"] || "No secret found";
+  res.send(`PAYLOAD_SECRET: ${secret}`);
+});
+
+app.get("/health", (_req: Request, res: Response) => {
+  res.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  res.set("Pragma", "no-cache");
+  res.set("Expires", "0");
+  res.set("Surrogate-Control", "no-store");
+  res.sendStatus(200);
+});
+
 app.listen(3000, () => {
   console.log("NODE_ENV:", process.env["NODE_ENV"]);
 });
